386ea7268eea6dcb0bc27a5218b36750aa4b1b82
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-17 07:51:18 +1100

    Initial commit

---
+++ compare_two_dates/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * C# Program to Campare Two Dates
+ */
+
+using System;
+
+namespace compare_two_dates
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            DateTime sd = new DateTime(2010, 10, 12);
+            Console.WriteLine("Starting Date : {0}", sd);
+            DateTime ed = sd.AddDays(10);
+            Console.WriteLine("Ending Date   : {0}", ed);
+            if (sd < ed)
+                Console.WriteLine("{0} Occurs Before {1}", sd, ed);
+            Console.Read();
+            return 0;
+        }
+    }
+}
